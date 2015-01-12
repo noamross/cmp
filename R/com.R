ab0082a252eaa1b22caebc542345d3d2246b3311
Noam Ross <noam.ross@gmail.com>
2015-01-12 12:43:36 -0800

    Added option to specify initial guess for opt.fit

--- R/com.R
+++ R/com.R
@@ -7,6 +7,7 @@
 #' level and the second column is the count for the corresponding level.
 #' 
 #' @param x matrix of count data
+#' @param par initial guesses for \deqn{lambda} and \deqn{nu}
 #' @return Returns an object containing four fields: \item{lambda }{ Estimate
 #' of the lambda parameter} \item{nu }{ Estimate of the nu parameter} \item{z
 #' }{ Normalizing constant} \item{fitted.values }{ Estimated counts at given
@@ -24,11 +25,14 @@
 #' 	com.fit(Lemaire);
 #' 
 #' @export com.fit
-com.fit = function(x)
+com.fit = function(x, par=NULL)
 {
-	xbar = (x[,1] %*% x[,2]) / sum(x[,2]);
+	if (is.null(par)) {
+		xbar = (x[,1] %*% x[,2]) / sum(x[,2]);
+		par = c(xbar, 1)
+	}
 	options(warn = -1);
-	result = optim(c(xbar,1), function(p) {return (-com.loglikelihood(x, p[1], p[2]));},
+	result = optim(par, function(p) {return (-com.loglikelihood(x, p[1], p[2]));},
 		method="L-BFGS-B", lower=c(1e-10,0));
 	options(warn = 0);
 	
