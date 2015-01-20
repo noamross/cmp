1e6b743e53e776354c4f72f303c8ddc59b7c36e2
Noam Ross <noam.ross@gmail.com>
2015-01-20 14:37:58 -0800

    Adding tests

---
+++ tests/testthat/test-numerical-correctness.R
@@ -0,0 +1,18 @@
+context('Numerical Correctness')
+
+test_that("Distribution Functions Match", {
+  testvals = expand.grid(lambda=c(0.1, 1), nu=10^(-3:3))
+  for(i in 1:nrow(testvals)) {
+    eval(bquote(
+      expect_equal(
+        dcom(0:10, lambda = .(testvals$lambda[i]), nu = .(testvals$nu[i])), 
+        compoissonOld::dcom(0:10, lambda = .(testvals$lambda[i]), nu = .(testvals$nu[i])))))
+  }
+  
+  for(i in 1:nrow(testvals)) {
+    eval(bquote(
+      expect_equal(
+        pcom(10, lambda = .(testvals$lambda[i]), nu = .(testvals$nu[i])), 
+        sum(compoissonOld::dcom(0:10, lambda = .(testvals$lambda[i]), nu = .(testvals$nu[i]))))))
+  }
+})
