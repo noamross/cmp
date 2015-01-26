3e376e4f451ffbd92c431d96bf0c2c64ddaafcf3
Noam Ross <noam.ross@gmail.com>
2015-01-25 20:45:05 -0800

    Updates to tests

--- tests/testthat/test-numerical-correctness.R
+++ tests/testthat/test-numerical-correctness.R
@@ -1,8 +1,16 @@
 context('Numerical Correctness')
 
 test_that("Distribution Functions Match", {
-  testvals = expand.grid(lambda=c(0.1, 1), nu=10^(-3:3))
+  testvals = expand.grid(lambda=seq(0.1, 1.9, by=0.2), nu=seq(0.1, 1.9, by=0.2))
   for(i in 1:nrow(testvals)) {
+    eval(bquote(
+      expect_equal(
+        com_compute_log_z(lambda = .(testvals$lambda[i]), nu = .(testvals$nu[i])), 
+        compoissonOld::com.compute.log.z(lambda = .(testvals$lambda[i]), nu = .(testvals$nu[i])))))
+  }
+  
+  for(i in 1:nrow(testvals)) {
+    
     eval(bquote(
       expect_equal(
         dcom(0:10, lambda = .(testvals$lambda[i]), nu = .(testvals$nu[i])), 
@@ -24,4 +32,10 @@ test_that("Distribution Functions Match", {
   }
   
 })
+
+test_that("pcom is bounded", {
+  testvals = expand.grid(lambda=seq(0.1, 1.9, by=0.2), nu=seq(0.1, 1.9, by=0.2))
+  for(i in 1:nrow(testvals)) {
+    eval(bquote(
+      expect_less_than(pcom(1000, lambda = .(testvals$lambda[i]), nu = .(testvals$nu[i])), 1.000001))) }})
 
